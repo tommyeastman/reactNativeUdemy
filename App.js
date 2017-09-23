598b3c6db5c14946a81a4912866df294c97b5f38
tommyeastman1 <tommyeastman1@gmail.com>
2017-09-23 17:04:01 -0300

    Get rid of view tags and clean up a bit. This is now the bare minimum code needed to render text to screen with expo.

--- App.js
+++ App.js
@@ -3,20 +3,14 @@
 import React from 'react';
 
 //import just the Text property from the react-native library
-//import { Text, AppRegistry } from 'react-native';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 
-// Create a component.
+// Create a component and render it to the screen.
 //tell ReactNative that we want to render some text to the screen of our device.
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Some Text.</Text>
-      </View>
+        <Text>Some text.</Text>
     );
   }
 }
-
-// Render it to the device.
-//AppRegistry.registerComponent('my-app', () => App);
