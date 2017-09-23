09b57363f8c35b710b11697d36b5a82df483c973
tommyeastman1 <tommyeastman1@gmail.com>
2017-09-23 16:52:08 -0300

    successfully print to screen

--- App.js
+++ App.js
@@ -4,28 +4,19 @@ import React from 'react';
 
 //import just the Text property from the react-native library
 //import { Text, AppRegistry } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Create a component.
 //tell ReactNative that we want to render some text to the screen of our device.
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text>Some Text.</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 // Render it to the device.
 //AppRegistry.registerComponent('my-app', () => App);
